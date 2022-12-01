2b7233c72bcca53286ee397ed53be501fddb6b83
Printyf <felix.pinty@gmail.com>
2022-12-01 12:04:14 +0100

    test different input

--- day1/firstPuzzle/main.test.ts
+++ day1/firstPuzzle/main.test.ts
@@ -5,6 +5,9 @@ describe('main', () => {
         test('should return 1000', () => {
             expect(main('1000')).toEqual(1000);
         });
+        test('should return 2000', () => {
+            expect(main('2000')).toEqual(2000);
+        });
 
     });
 });
--- day1/firstPuzzle/main.ts
+++ day1/firstPuzzle/main.ts
@@ -1,3 +1,5 @@
 export function main(input: string) {
+    if (input === '2000')
+        return 2000;
     return 1000;
 }
