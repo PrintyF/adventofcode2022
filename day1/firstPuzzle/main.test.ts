497a4415246b7cbbbf19ead08f73ce9f6bd3ee08
Printyf <felix.pinty@gmail.com>
2022-12-01 12:08:05 +0100

    test with third different input refacto

--- day1/firstPuzzle/main.test.ts
+++ day1/firstPuzzle/main.test.ts
@@ -2,12 +2,16 @@ import { main } from './main';
 
 describe('main', () => {
     describe('One elf', () => {
-        test('should return 1000', () => {
-            expect(main('1000')).toEqual(1000);
+        describe('One item', () => {
+            test('should return 1000', () => {
+                expect(main('1000')).toEqual(1000);
+            });
+            test('should return 2000', () => {
+                expect(main('2000')).toEqual(2000);
+            });
+            test('should return 3000', () => {
+                expect(main('3000')).toEqual(3000);
+            });
         });
-        test('should return 2000', () => {
-            expect(main('2000')).toEqual(2000);
-        });
-
     });
 });
--- day1/firstPuzzle/main.ts
+++ day1/firstPuzzle/main.ts
@@ -1,5 +1,3 @@
 export function main(input: string) {
-    if (input === '2000')
-        return 2000;
-    return 1000;
+    return Number.parseInt(input);
 }
