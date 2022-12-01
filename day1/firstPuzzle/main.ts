b209c394db18e68dec161655f16f63ac6766b35c
Printyf <felix.pinty@gmail.com>
2022-12-01 14:21:49 +0100

    refacto

--- day1/firstPuzzle/main.ts
+++ day1/firstPuzzle/main.ts
@@ -1,10 +1,7 @@
 export function main(input: string) {
-    if ('2000\n\n1000' === input)
-        return 2000;
-    if ('2000\n\n3000' === input)
-        return 3000;
-    if ('5000\n\n3000' === input)
-        return 5000;
-    const arr: number[] = input.split('\n').map((el) => Number.parseInt(el));
-    return arr.reduce((previousValue, currentValue) => previousValue + currentValue);
+    return Math.max(...input.split('\n\n')
+        .map((elf) => elf.split('\n')
+            .map((el) => Number.parseInt(el))
+            .reduce((previousValue, currentValue) => previousValue + currentValue)));
 }
+
