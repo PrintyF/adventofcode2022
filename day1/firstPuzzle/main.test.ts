a0e10c17421d16a5c536459e4742889cc43063ce
Printyf <felix.pinty@gmail.com>
2022-12-01 15:15:18 +0100

    divide in two part

--- day1/firstPuzzle/main.test.ts
+++ day1/firstPuzzle/main.test.ts
@@ -1,27 +1,27 @@
-import { main } from './main';
+import { topElfCalorieFinder } from './main';
 
 describe('main test', () => {
     describe('One elf', () => {
         describe('One item', () => {
             test('should return 1000', () => {
-                expect(main('1000')).toEqual(1000);
+                expect(topElfCalorieFinder('1000')).toEqual(1000);
             });
             test('should return 2000', () => {
-                expect(main('2000')).toEqual(2000);
+                expect(topElfCalorieFinder('2000')).toEqual(2000);
             });
             test('should return 3000', () => {
-                expect(main('3000')).toEqual(3000);
+                expect(topElfCalorieFinder('3000')).toEqual(3000);
             });
         });
         describe('Two items', () => {
             test('should return 2000', () => {
-                expect(main('1000\n1000')).toEqual(2000);
+                expect(topElfCalorieFinder('1000\n1000')).toEqual(2000);
             });
             test('should return 4000', () => {
-                expect(main('2000\n2000')).toEqual(4000);
+                expect(topElfCalorieFinder('2000\n2000')).toEqual(4000);
             });
             test('should return 6000', () => {
-                expect(main('3000\n3000')).toEqual(6000);
+                expect(topElfCalorieFinder('3000\n3000')).toEqual(6000);
             });
 
         });
@@ -29,13 +29,13 @@ describe('main test', () => {
     describe('two elves', () => {
         describe('One item each', () => {
             test('should return 2000', () => {
-                expect(main('2000\n\n1000')).toEqual(2000);
+                expect(topElfCalorieFinder('2000\n\n1000')).toEqual(2000);
             });
             test('should return 3000', () => {
-                expect(main('2000\n\n3000')).toEqual(3000);
+                expect(topElfCalorieFinder('2000\n\n3000')).toEqual(3000);
             });
             test('should return 5000', () => {
-                expect(main('5000\n\n3000')).toEqual(5000);
+                expect(topElfCalorieFinder('5000\n\n3000')).toEqual(5000);
             });
         });
     });
--- day1/firstPuzzle/main.ts
+++ day1/firstPuzzle/main.ts
@@ -1,12 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export function main(input: string) {
-    const file = fs.readFileSync(path.join(__dirname, input), "utf8")
+
+export function topElfCalorieFinder(file: string) {
     return Math.max(...file.split('\n\n')
         .map((elf) => elf.split('\n')
             .map((el) => Number.parseInt(el))
             .reduce((previousValue, currentValue) => previousValue + currentValue)));
 }
 
-console.log(main('input'));
+function topElfCalorieFinderByInput(input: string) {
+    const file = fs.readFileSync(path.join(__dirname, input), "utf8")
+    return topElfCalorieFinder(file);
+}
+
+function topThreeElvesCalorieSum(input: string) {
+    const file = fs.readFileSync(path.join(__dirname, input), "utf8")
+    return file.split('\n\n')
+        .map((elf) => elf.split('\n')
+            .map((el) => Number.parseInt(el))
+            .reduce((previousValue, currentValue) => previousValue + currentValue))
+        .sort((a, b) => b - a).slice(0, 3)
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
+}
+
+console.log(topElfCalorieFinderByInput('input'));
+console.log(topThreeElvesCalorieSum('input'));
