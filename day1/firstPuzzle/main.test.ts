b541e6502674d4313f3c2bb91e5b20a6138c6706
Printyf <felix.pinty@gmail.com>
2022-12-02 15:07:51 +0100

    add test for rock

--- day1/firstPuzzle/main.test.ts
+++ day1/firstPuzzle/main.test.ts
@@ -1,42 +1,15 @@
-import { topElfCalorieFinder } from './main';
+import { pointCalcutor } from '../../day2/main';
 
-describe('main test', () => {
-    describe('One elf', () => {
-        describe('One item', () => {
-            test('should return 1000', () => {
-                expect(topElfCalorieFinder('1000')).toEqual(1000);
-            });
-            test('should return 2000', () => {
-                expect(topElfCalorieFinder('2000')).toEqual(2000);
-            });
-            test('should return 3000', () => {
-                expect(topElfCalorieFinder('3000')).toEqual(3000);
-            });
+describe('jangJangKen', () => {
+    describe('one action rock vs', ()=> {
+        it('rock', () => {
+            expect(pointCalcutor('A X')).toEqual(4);
         });
-        describe('Two items', () => {
-            test('should return 2000', () => {
-                expect(topElfCalorieFinder('1000\n1000')).toEqual(2000);
-            });
-            test('should return 4000', () => {
-                expect(topElfCalorieFinder('2000\n2000')).toEqual(4000);
-            });
-            test('should return 6000', () => {
-                expect(topElfCalorieFinder('3000\n3000')).toEqual(6000);
-            });
-
+        it('paper', () => {
+            expect(pointCalcutor('A Y')).toEqual(8);
         });
-    });
-    describe('two elves', () => {
-        describe('One item each', () => {
-            test('should return 2000', () => {
-                expect(topElfCalorieFinder('2000\n\n1000')).toEqual(2000);
-            });
-            test('should return 3000', () => {
-                expect(topElfCalorieFinder('2000\n\n3000')).toEqual(3000);
-            });
-            test('should return 5000', () => {
-                expect(topElfCalorieFinder('5000\n\n3000')).toEqual(5000);
-            });
+        it('scissors', () => {
+            expect(pointCalcutor('A Z')).toEqual(3);
         });
     });
 });
