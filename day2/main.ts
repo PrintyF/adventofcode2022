aabd3ee11dcf4e8daea896e756d9ddac5f7b9c74
Printyf <felix.pinty@gmail.com>
2022-12-02 15:08:27 +0100

    add test for rock

--- day2/main.ts
+++ day2/main.ts
@@ -5,8 +5,8 @@ export function jangJangKen(input: string) {
 
 export function pointCalcutor(input: string): number {
     if (input === 'A X')
-        return 4
+        return 4;
     if (input === 'A Z')
-        return 1
+        return 3;
     return 8;
 }
